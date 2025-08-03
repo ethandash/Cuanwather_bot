7d540fd98f257451120ee74fda5511cfbdff938a
ethandash <ethan1050505@gmail.com>
2025-08-03 20:55:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 QWEN_API_KEY = "LTAI5tXXXXXXXX:YYYYYYYYYYYY"  # Ganti dengan RAM AccessKey kamu
 QWEN_BASE_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
 QWEN_MODEL = "qwen-turbo"
-
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+import os
+QWEN_API_KEY = os.getenv("QWEN_API_KEY")
